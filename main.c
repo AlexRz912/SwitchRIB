2f25a73b36776b85cf34f2bb6c3feb45ff503664
Alexandre Rodriguez <alexandre.rodriguez.alrz@gmail.com>
2026-03-05 23:18:38 +0100

    code to add arbitrary string to clipboard

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-#include "./keyboard/handler.h"
+#include <Windows.h>
 
 int main() {
+    const char* output = "qsdfqsdffqsfdqsfdqf";
+    const size_t len = strlen(output) + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), output, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
     return 0;
 }
